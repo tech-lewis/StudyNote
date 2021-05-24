c35d899fee65f80845579a3c18410258e72038d3
MarkLewis <techliujing@gmail.com>
2021-05-24 17:28:48 +0800

    add src for reactDom simple render

---
+++ react_fiber/src/simpleReact/Dom.js
@@ -0,0 +1,21 @@
+function appendChildren(node, children) {  // 多节点和单节点的兼容
+    if (Array.isArray(children)) {
+        children.forEach((child) => node.appendChild(child))
+    } else {
+        node.appendChild(children);
+    }
+}
+
+function empty(node) {
+    [].slice.call(node.childNodes).forEach((child) => {
+        node.removeChild(child);
+    });
+}
+
+
+export default {
+    appendChildren,
+    empty
+}
+
+/// 实用化的Dom工具
