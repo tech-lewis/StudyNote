d413a962a320b85a9f61a27d49ba65c5627489b8
MarkLewis <marklewis@qq.com>
2023-02-18 22:55:13 +0800

    delete react projects

--- RN with Typescript/src/constant.ts
+++
@@ -1,6 +0,0 @@
-const constant = {
-  healthIcon: "health-icon",
-  reservationNoticeUri: "http://47.92.145.208/pages/orderNotice.html"
-};
-
-export default constant;
--- RN with Typescript/src/utils/getScreenWidth.ts
+++
@@ -1,4 +0,0 @@
-import { Dimensions } from "react-native";
-
-const getScreenWidth: () => number = () => Dimensions.get("window").width;
-export default getScreenWidth;
